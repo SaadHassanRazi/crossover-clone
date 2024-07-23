0c5ac33463429d92fce63d2034fee3aa92a2c4ed
Saim Waleed <147861158+saym-waleed@users.noreply.github.com>
2024-07-23 22:28:00 +0500

    header commit

--- src/interface/components/header/Header.jsx
+++ src/interface/components/header/Header.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import headerVideo from "../../../assets/video/2516159-hd_1920_1080_24fps.mp4";
+import "./Header.css"; // Assuming you have a CSS file for styling
+
 const Header = () => {
   return (
     <div className="main">
-      <video src={headerVideo} style={{ width: "100%" }} autoPlay muted loop />
+      <div className="video-container">
+        <video
+          src={headerVideo}
+          style={{ width: "100%" }}
+          autoPlay
+          muted
+          loop
+        />
+        <div className="text-overlay">
+          <h1>
+            Are <b>You</b> The Best?
+          </h1>
+          <p>
+            We enable talented professionals around the world to qualify for
+            awesome remote jobs—with Silicon Valley pay. Ready to prove your
+            skills?
+          </p>
+              
+            <button className="button">FIND A REMOTE FULL-TIME JOB </button>
+         
+        </div>
+      </div>
     </div>
   );
 };
